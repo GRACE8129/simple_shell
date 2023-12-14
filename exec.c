0219da34bee9123a42c0c88eba4074d76a90eccc
athaka <jesoka18@gmail.com>
2023-12-14 13:59:22 -0800

    updated execute function

--- exec.c
+++ exec.c
@@ -1,30 +1,48 @@
 #include "shell.h"
 
 /**
-  * execute_input - executes a giving input command.
-  *  @input: The command to be executed.
-  */
-
+ * execute_input - executes a given input command.
+ * @input: The command to be executed.
+ */
 void execute_input(const char *input)
 {
-	pid_t child_pid = fork();
+	pid_t child_pid;
+	int status;
 
+	/* Fork a child process */
+	child_pid = fork();
 	if (child_pid == -1)
-
 	{
 		perror("fork");
 		exit(EXIT_FAILURE);
 	}
 	else if (child_pid == 0)
 	{
-		if (execlp("/bin/sh", "sh", "-c", input, (char *)NULL) == -1)
-		{
-			perror("execlp");
-			exit(EXIT_FAILURE);
-		}
+		/* Child process */
+		/* Tokenize the input command */
+		char *arguments[MAX_NUM_ARGS];
+		execute_external(input, arguments);
+		/* Attempt to execute the command */
+		execvp(arguments[0], arguments);
+		/* If execvp fails, print an error message */
+		perror("execvp");
+		exit(EXIT_FAILURE);
 	}
 	else
 	{
-		wait(NULL);
+		/* Parent process */
+		/* Wait for the child to complete */
+		waitpid(child_pid, &status, 0);
+		/* Check if the child process exited normally */
+		if (WIFEXITED(status))
+		{
+			/* Print the exit status if needed */
+			printf("Child exited with status %d\n", WEXITSTATUS(status));
+		}
+		else if (WIFSIGNALED(status))
+		{
+			/* Print information about the signal that caused the child to terminate */
+			printf("Child process terminated by signal %d\n", WTERMSIG(status));
+		}
 	}
 }
